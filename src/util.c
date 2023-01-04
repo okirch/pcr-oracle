d0ffafbdb52f3cd52e10da385d6cf1268254524d
Olaf Kirch <okir@suse.com>
2023-01-04 12:36:16 +0100

    Copyright statement fixes
    Signed-off-by: Olaf Kirch <okir@suse.com>

--- src/util.c
+++ src/util.c
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2022 SUSE LLC
+ *   Copyright (C) 2022, 2023 SUSE LLC
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
